b7f41283bacc42d926047ec31bf355c5091116a4
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-05-28 06:11:50 +0900

    maypage design

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -11,6 +11,42 @@ class MyPage extends React.Component {
 		<div className="user-info">
 		  <img src={`http://furyu.nazo.cc/twicon/${Cookies.get('screen_name')}/original`} />
 		  <p className="sn">@{Cookies.get('screen_name')}</p>
+		  <p className="flag">
+		  	<i className="ion-flag"/>
+		  	<span>今まで0個の聖地を訪れました</span>
+		  </p>
+		</div>
+		<div className="history">
+			<a>
+		  		<div className="icon">
+		  			<i className="ion-flag"/>
+				</div>
+		  		<div className="data">
+		  			<h2>聖地の名前聖地の名前聖地の名前</h2>
+		  			<p>アニメの名前</p>
+		  			<p>1999/4/10</p>
+				</div>
+			</a>
+			<a>
+		  		<div className="icon">
+		  			<i className="ion-flag"/>
+				</div>
+		  		<div className="data">
+		  			<h2>聖地の名前聖地の名前聖地の名前</h2>
+		  			<p>アニメの名前</p>
+		  			<p>1999/4/10</p>
+				</div>
+			</a>
+			<a>
+		  		<div className="icon">
+		  			<i className="ion-flag"/>
+				</div>
+		  		<div className="data">
+		  			<h2>聖地の名前聖地の名前聖地の名前</h2>
+		  			<p>アニメの名前</p>
+		  			<p>1999/4/10</p>
+				</div>
+			</a>
 		</div>
       </main>
   }
