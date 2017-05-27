ac37a439cc4d116eda24c9265cfd8e115f8bbfa0
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-05-28 05:45:15 +0900

    menu実装

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import {container} from '../utils'
 
+class Header extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			isOpen: false
+		}
+	}
 
-export default container(() =>
-	<div className="header">
-		<p>Hello</p>
+ 	render() {
+	return <div className="wrap">
+		<div className="header" >
+			<p>Hello</p>
+			<i className="ion-navicon menu-icon" onClick={ () => this.toggle() }/>
+		</div>
+		<div className={this.state.isOpen ? 'menu active' : 'menu'}>
+			<a>
+				<i className="ion-bonfire"/>
+				<span>タイムライン</span>
+			</a>
+			<a href="/mypage">
+				<i className="ion-person"/>
+				<span>マイページ</span>
+			</a>
+			<a href="/favorite-anime-config">
+				<i className="ion-gear-a"/>
+				<span>設定</span>
+			</a>
+		</div>
 	</div>
-)
+  	}
+
+	toggle(){
+		console.log('toggle')
+		this.setState({isOpen: !this.state.isOpen})
+	}
+
+}
+
+export default container(Header)
