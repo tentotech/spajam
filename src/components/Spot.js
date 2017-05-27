3bcfda8d49846d5c8157ecdd4e7ec63a87cc452d
Sosuke Suzuki <aosukeke@gmail.com>
2017-05-28 07:54:17 +0900

    ボタンでmapに飛ぶようにしました。

--- src/components/Spot.js
+++ src/components/Spot.js
@@ -11,6 +11,12 @@ class Spot extends React.Component {
     }
   }
 
+  handleClick() {
+    const latitude = this.props.main.sacredPlace.sacred_place.latitude
+    const longitude = this.props.main.sacredPlace.sacred_place.longitude
+    location.href = `https://www.google.co.jp/maps/@${latitude},${longitude},15z`
+  }
+
   render() {
     return <main className="spot">
       <div className="cover" style={{'backgroundImage': `url("http://blog-imgs-92.fc2.com/b/l/o/blogman55/livejupiter_1461993406_12701.jpg")`}}>
@@ -36,7 +42,7 @@ class Spot extends React.Component {
         <p>ここを訪れたユーザー</p>
         {this.props.main.sacredPlace.users.map(user => <img src={`http://furyu.nazo.cc/twicon/${user.screen_name}/normal`}/>)}
       </div>
-      <div className="googleMap">
+      <div className="googleMap" onClick={this.handleClick.bind(this)}>
         <p>
           <i className="ion-location"/>
           <span>Google Map で見る</span>
