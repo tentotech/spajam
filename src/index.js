b3132139d375c9c4f3a4a74afc3d404022389d78
wh11e <wh11e7rue@icloud.com>
2017-05-27 15:31:14 +0900

    Copy src/index.js from wh11e/frontend.g8 and remove type difinitions and redux-observable dependency

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import {render} from 'react-dom'
+import {browserHistory} from 'react-router'
+import {AppContainer} from 'react-hot-loader'
+import {createStore, applyMiddleware} from 'redux'
+import {syncHistoryWithStore, routerMiddleware} from 'react-router-redux'
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly'
+import * as _ from 'lodash'
+import App from './components'
+import {reducer} from './reducers'
+
+
+const middleware = composeWithDevTools(applyMiddleware(routerMiddleware(browserHistory)))
+const store = createStore(reducer, middleware)
+const history = syncHistoryWithStore(browserHistory, store)
+
+function renderHMR(App) {
+  render(
+    <AppContainer>
+      <App store={store} history={history} />
+    </AppContainer>,
+    document.getElementsByTagName('div')[0]
+  )
+}
+renderHMR(App)
+
+if (module.hot) {
+  module.hot.accept('./components', () => {
+    const App = require('./components').default
+    renderHMR(App)
+  })
+
+  module.hot.accept('./reducers', () => {
+    const reducer = require('./reducers').reducer
+    store.replaceReducer(reducer)
+  })
+}
