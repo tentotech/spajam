f2145b388ae2f905b60adf510950b49734f2d985
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-05-28 01:57:24 +0900

    ログイン前のデザイン

--- express-sql/sql.js
+++ express-sql/sql.js
@@ -16,6 +16,7 @@ connection.connect();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	next()
