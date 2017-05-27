2437867ac7cef1a231c3ad3c9694d9167ce994cb
wh11e <wh11e7rue@icloud.com>
2017-05-27 19:37:21 +0900

    Use js-cookie

--- src/actions/main.js
+++ src/actions/main.js
@@ -1,9 +1,10 @@
+import Cookies from 'js-cookie'
 import {withLabels} from './utils'
 
 
 export const actions = withLabels({
   SIGNIN: screen_name => {
-    document.cookie = `screen_name=${screen_name}`
+    Cookies.set('screen_name', screen_name)
     return {screen_name}
   }
 })
