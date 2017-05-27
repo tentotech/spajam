f5d4faf40c4513a0aeb95549547d2ce396ff531e
wh11e <wh11e7rue@icloud.com>
2017-05-27 18:27:30 +0900

    Implement Top component

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,18 +1,20 @@
 import * as React from 'react'
 import {container} from '../utils'
-
+import OAuth from 'oauth'
 
 
 class Top extends React.Component {
   render() {
-    return <main className='top' onClick={this.handleClick.bind(this)}>
-      {`Hello ${this.props.main.greetingsTo}`}
+    return <main className='top'>
+      <a onClick={this.signin.bind(this)}>Sign in with Twitter</a>
     </main>
   }
 
-  handleClick() {
-    debugger
-    this.props.main.changeTheWorld(123)
+  signin() {
+    OAuth.poup('twitter')
+      .done(result => {
+        this.props.main.signin(result.me().name)
+      }).fail(err => console.error(err))
   }
 }
 
